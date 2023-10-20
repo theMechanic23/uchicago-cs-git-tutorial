3d167495a8328e544b4f4e72a685069f22b237b5
Junsung Kim <juliuskim1012@linux1.cs.uchicago.edu>
2023-10-20 15:10:22 -0500

    Updated buffer size to 1000

--- echo.c
+++ echo.c
@@ -8,7 +8,7 @@
 
 int main(){
     char input[500];
-    while(fgets(input, 500, stdin)){  //read from STDIN (aka command-line)
+    while(fgets(input, 1000, stdin)){  //read from STDIN (aka command-line)
         printf("%s\n", input);  //print out what user typed in
         memset(input, 0, strlen(input));  //reset string to all 0's
     }
